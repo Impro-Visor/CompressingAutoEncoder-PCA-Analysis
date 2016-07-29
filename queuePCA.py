4353a71796e9fa43a96fdd208f85c2564e310648
Nicholas Weintraut <weintraun8@students.rowan.edu>
2016-07-28 18:43:20 -0700

    Add support for mapping interp features on top of input corpus features to show paths

--- queuePCA.py
+++ queuePCA.py
@@ -24,7 +24,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-point_color = 'Set3'
+point_color = 'viridis'
 background = 'white'
 border = 'none'
 
@@ -33,23 +33,38 @@ axes = []
 #############################################
 interpFeatureMatrix = np.loadtxt('interp_feature_matrix', delimiter=',')
 interpFeatureMatrix_PCA = results.project(sp.special.logit(interpFeatureMatrix))
-interpGraphData_PCA = np.array(np.split(interpFeatureMatrix_PCA,8))
 
-#print interpGraphData_PCA.ndim
+numbers = enumerate(interpFeatureMatrix)
 
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111, projection='3d', axisbg = background)
-ax1.set_title('Interpolation Feature Matrix with PCA')
-#colors = [element for element in interpGraphData[:,2]]
-colors = [i % 8 for i in range(len(interpGraphData_PCA[:,2]))]
-branno = ax1.scatter(interpGraphData_PCA[:,0], interpGraphData_PCA[:,1], interpGraphData_PCA[:,2], cmap=point_color, marker='o', edgecolors='none')
-ax1.set_xlabel('axis 0')
-ax1.set_ylabel('axis 1')
-ax1.set_zlabel('axis 2')
-#plt.colorbar(branno)
+interpGraphData_PCA = np.array(np.split(interpFeatureMatrix_PCA,5)) #dimensions = interpolationStep, queueFeatureIndex, principal component mapping
+
+def plot_interp_state(xVals, yVals, zVals, i):
+	fig1 = plt.figure()
+	ax = fig1.add_subplot(111, projection='3d', axisbg = background)
+	ax.set_title('Interpolation Feature Matrix with PCA (' + str(i) + ')')
+
+	sustain_values = np.loadtxt("features_property_values_sustain.v", delimiter=",")
+	colors = [element for element in sustain_values]
+	boop = ax.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', alpha=0.04, edgecolors=border)
+	branno = ax.scatter(xVals, yVals, zVals, c='red', marker='o', edgecolors='none')
+	# i, data in numbers:
+		#print(interpFeatureMatrix_PCA[i,:3])
+		#ax1.text(interpFeatureMatrix_PCA[i,0], interpFeatureMatrix_PCA[i,1], interpFeatureMatrix_PCA[i,2], i) #interpFeatureMatrix_PCA[i,:3], i
+	ax.set_xlabel('axis 0')
+	ax.set_ylabel('axis 1')
+	ax.set_zlabel('axis 2')
+	plt.colorbar(boop)
+
+perInterp = True
+
+if perInterp:
+	for i in range(8):
+		plot_interp_state(interpGraphData_PCA[:,i,0], interpGraphData_PCA[:,i,1], interpGraphData_PCA[:,i,2], i)
+else:
+	for i in range(1):
+		plot_interp_state(interpGraphData_PCA[i,:,0], interpGraphData_PCA[i,:,1], interpGraphData_PCA[i,:,2], i)
 
 plt.show()
-
 #############################################
 '''fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d', axisbg = background)
@@ -88,25 +103,25 @@ plt.colorbar(boop)'''
 #######################################
 def plot_figure_colors(title, data_list, input_file, xlabel = 'PCA[0] magnitude', ylabel = 'PCA[1] magnitude', zlabel = 'PCA[2] magnitude'):
 	fig = plt.figure()
-	ax = fig2.add_subplot(111, projection='3d', axisbg = background)
+	ax = fig.add_subplot(111, projection='3d', axisbg = background)
 	ax.set_title(title)
 	plotted_values = np.loadtxt(input_file, delimiter=",")
 	colors = [element for element in plotted_values]
-	boop = ax2.scatter(data_list[:,0], data_list[:,1], data_list[:,2], c=colors, cmap=point_color, marker='o', edgecolors=border)
-	ax2.set_xlabel(xlabel)
-	ax2.set_ylabel(ylabel)
-	ax2.set_zlabel(zlabel)
+	boop = ax.scatter(data_list[:,0], data_list[:,1], data_list[:,2], c=colors, cmap=point_color, marker='o', edgecolors=border)
+	ax.set_xlabel(xlabel)
+	ax.set_ylabel(ylabel)
+	ax.set_zlabel(zlabel)
 	plt.colorbar(boop)
 	plt.show()
 
 def plot_figure(title, data_list, input_file, xlabel = 'PCA[0] magnitude', ylabel = 'PCA[1] magnitude', zlabel = 'PCA[2] magnitude'):
 	fig = plt.figure()
-	ax = fig2.add_subplot(111, projection='3d', axisbg = background)
+	ax = fig.add_subplot(111, projection='3d', axisbg = background)
 	ax.set_title(title)
-	boop = ax2.scatter(data_list[:,0], data_list[:,1], data_list[:,2], cmap=point_color, marker='o', edgecolors=border)
-	ax2.set_xlabel(xlabel)
-	ax2.set_ylabel(ylabel)
-	ax2.set_zlabel(zlabel)
+	boop = ax.scatter(data_list[:,0], data_list[:,1], data_list[:,2], cmap=point_color, marker='o', edgecolors=border)
+	ax.set_xlabel(xlabel)
+	ax.set_ylabel(ylabel)
+	ax.set_zlabel(zlabel)
 	plt.colorbar(boop)
 	plt.show()
 
