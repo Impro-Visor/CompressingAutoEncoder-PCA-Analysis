e9a7d92d1025a4ba5e7b97ce2033d2d38b00678e
MackenzieKong-Sivert <mkong.sivert@gmail.com>
2016-08-01 15:04:47 -0700

    Adds function for plotting all points in interpolation feature matrix and cleans up code
    Where previously the old graphs were just commented out, now the
    generation of graphs has been put into functions. There is also a new
    function that generates a graph with all the points in the interpolation
    feature matrix.

--- queuePCA.py
+++ queuePCA.py
@@ -31,14 +31,21 @@ border = 'none'
 axes = []
 
 #############################################
-interpFeatureMatrix = np.loadtxt('interp_feature_matrix', delimiter=',')
-interpFeatureMatrix_PCA = results.project(sp.special.logit(interpFeatureMatrix))
+'''
+Interpolation Graph Functions
+'''
+def load_interp_files():
+	'''loads all the files and such needed for the interpolation graph functions'''
+	interpFeatureMatrix = np.loadtxt('interp_feature_matrix', delimiter=',')
+	interpFeatureMatrix_PCA = results.project(sp.special.logit(interpFeatureMatrix))
 
-numbers = enumerate(interpFeatureMatrix)
+	numbers = enumerate(interpFeatureMatrix)
 
-interpGraphData_PCA = np.array(np.split(interpFeatureMatrix_PCA,5)) #dimensions = interpolationStep, queueFeatureIndex, principal component mapping
+	interpGraphData_PCA = np.array(np.split(interpFeatureMatrix_PCA,5)) #dimensions = interpolationStep, queueFeatureIndex, principal component mapping
 
-def plot_interp_state(xVals, yVals, zVals, i):
+def plot_interp_state(xVals, yVals, zVals, i, showGraph = True):
+	'''plots the interpolation state on top of the original graph'''
+	load_interp_files()
 	fig1 = plt.figure()
 	ax = fig1.add_subplot(111, projection='3d', axisbg = background)
 	ax.set_title('Interpolation Feature Matrix with PCA (' + str(i) + ')')
@@ -50,58 +57,141 @@ def plot_interp_state(xVals, yVals, zVals, i):
 	# i, data in numbers:
 		#print(interpFeatureMatrix_PCA[i,:3])
 		#ax1.text(interpFeatureMatrix_PCA[i,0], interpFeatureMatrix_PCA[i,1], interpFeatureMatrix_PCA[i,2], i) #interpFeatureMatrix_PCA[i,:3], i
-	ax.set_xlabel('axis 0')
-	ax.set_ylabel('axis 1')
-	ax.set_zlabel('axis 2')
+	ax.set_xlabel('Interpolation Step')
+	ax.set_ylabel('Queue Feature Index')
+	ax.set_zlabel('Principal Component Mapping')
 	plt.colorbar(boop)
+	if showGraph:
+		plt.show()
+	else:
+		plt.savefig('interp_state.png')
 
-perInterp = True
+def whole_interp_graph(showGraph = True):
+	'''plots all the points in the interpolation feature matrix'''
+	load_interp_files()
+	fig0 = plt.figure()
+	ax = fig0.add_subplot(111, projection='3d', axisbg=background)
+	ax.set_title('Interpolation Feature Matrix with PCA')
+	branno = ax.scatter(interpGraphData_PCA[:,0], interpGraphData_PCA[:,1], interpGraphData_PCA[:,2])
+	ax.set_xlabel('Interpolation Step')
+	ax.set_ylabel('Queue Feature Index')
+	ax.set_zlabel('Principal Component Mapping')
+	if showGraph:
+		plt.show()
+	else:
+		plt.savefig('interp_feature_matrix.png')
 
-if perInterp:
-	for i in range(8):
-		plot_interp_state(interpGraphData_PCA[:,i,0], interpGraphData_PCA[:,i,1], interpGraphData_PCA[:,i,2], i)
-else:
-	for i in range(1):
-		plot_interp_state(interpGraphData_PCA[i,:,0], interpGraphData_PCA[i,:,1], interpGraphData_PCA[i,:,2], i)
+def interp_graphs(perInterp = True, showGraph = True):
+	'''calls plot_interp_state for each feature'''
+	if perInterp:
+		for i in range(8):
+			plot_interp_state(interpGraphData_PCA[:,i,0], interpGraphData_PCA[:,i,1], interpGraphData_PCA[:,i,2], i)
+			if showGraph:
+				plt.show()
+			else:
+				plt.savefig('interp_state' + str(i) + '.png')
+	else:
+		for i in range(1):
+			plot_interp_state(interpGraphData_PCA[i,:,0], interpGraphData_PCA[i,:,1], interpGraphData_PCA[i,:,2], i)
+			if showGraph:
+				plt.show()
+			else:
+				plt.savefig('interp_state' + str(i) + '.png')
 
-plt.show()
 #############################################
-'''fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d', axisbg = background)
-ax.set_title("Percentage of sustaining time steps")
-rest_values = np.loadtxt("features_property_values_sustain.v", delimiter=",")
-colors = [element for element in rest_values[:fourth]]
 
-boop = ax.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
-ax.set_xlabel('PCA[0] magnitude')
-ax.set_ylabel('PCA[1] magnitude')
-ax.set_zlabel('PCA[2] magnitude')
+def absurdly_many_graphs():
+	'''In the interest of being thorough, this generates 3D graphs for all the components'''
+	for i in range(98):
+		data = results.Y[:,i:i+3]
+		fig = plt.figure()
+		ax = fig.add_subplot(111, projection='3d', axisbg = background)
+		ax.set_title("Percentage of resting time steps")
+		rest_values = np.loadtxt("features_property_values_rest.v", delimiter=",")
+		colors = [element for element in rest_values]
+		boop = ax.scatter(data[:,0], data[:,1], data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
+		ax.set_xlabel('PCA[' +str(i)+ '] magnitude')
+		ax.set_ylabel('PCA['+str(i+1)+'] magnitude')
+		ax.set_zlabel('PCA['+str(i+2)+'] magnitude')
+		plt.colorbar(boop)
+		plt.savefig('PCArest_dim' + str(i) + 'through' + str(i+2) + '.png')
+		#############################################
+		fig2 = plt.figure()
+		ax1 = fig2.add_subplot(111, projection='3d', axisbg = background)
+		ax1.set_title("Percentage of sustaining time steps")
+		sustain_values = np.loadtxt("features_property_values_sustain.v", delimiter=",")
+		colors = [element for element in sustain_values]
+		boop = ax1.scatter(data[:,0], data[:,1], data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
+		ax.set_xlabel('PCA['+ str(i) +'] magnitude')
+		ax.set_ylabel('PCA['+ str(i+1) + '] magnitude')
+		ax.set_zlabel('PCA['+ str(i+2) + '] magnitude')
+		plt.colorbar(boop)
+		plt.savefig('PCAsust_dim' + str(i) + 'through' + str(i+2) + '.png')
+		##########################################
+		fig2 = plt.figure()
+		ax2 = fig2.add_subplot(111, projection='3d', axisbg = background)
+		ax2.set_title("Percentage of articulating time steps")
+		articulate_values = np.loadtxt("features_property_values_articulate.v", delimiter=",")
+		colors = [element for element in articulate_values]
+		boop = ax2.scatter(data[:,0], data[:,1], data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
+		ax.set_xlabel('PCA['+ str(i) +'] magnitude')
+		ax.set_ylabel('PCA['+ str(i+1) + '] magnitude')
+		ax.set_zlabel('PCA['+ str(i+2) + '] magnitude')
+		plt.colorbar(boop)
+		plt.savefig('PCAart_dim' + str(i) + 'through' + str(i+2) + '.png')
 
-plt.colorbar(boop)
 #############################################
-fig2 = plt.figure()
-ax1 = fig2.add_subplot(111, projection='3d', axisbg = background)
-ax1.set_title("Percentage of sustaining time steps")
-sustain_values = np.loadtxt("features_property_values_sustain.v", delimiter=",")
-colors = [element for element in sustain_values]
-boop = ax1.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
-ax1.set_xlabel('PCA[0] magnitude')
-ax1.set_ylabel('PCA[1] magnitude')
-ax1.set_zlabel('PCA[2] magnitude')
-plt.colorbar(boop)
-##########################################
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111, projection='3d', axisbg = background)
-ax2.set_title("Percentage of articulating time steps")
-articulate_values = np.loadtxt("features_property_values_articulate.v", delimiter=",")
-colors = [element for element in articulate_values]
-boop = ax2.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
-ax2.set_xlabel('PCA[0] magnitude')
-ax2.set_ylabel('PCA[1] magnitude')
-ax2.set_zlabel('PCA[2] magnitude')
-plt.colorbar(boop)'''
+
+def original():
+	'''generates the original three graphs of the first three principal components'''
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d', axisbg = background)
+	ax.set_title("Percentage of resting time steps")
+	rest_values = np.loadtxt("features_property_values_rest.v", delimiter=",")
+	colors = [element for element in rest_values]
+	boop = ax.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
+	ax.set_xlabel('PCA[0] magnitude')
+	ax.set_ylabel('PCA[1] magnitude')
+	ax.set_zlabel('PCA[2] magnitude')
+	plt.colorbar(boop)
+	#############################################
+	fig2 = plt.figure()
+	ax1 = fig2.add_subplot(111, projection='3d', axisbg = background)
+	ax1.set_title("Percentage of sustaining time steps")
+	sustain_values = np.loadtxt("features_property_values_sustain.v", delimiter=",")
+	colors = [element for element in sustain_values]
+	boop = ax1.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
+	ax1.set_xlabel('PCA[0] magnitude')
+	ax1.set_ylabel('PCA[1] magnitude')
+	ax1.set_zlabel('PCA[2] magnitude')
+	plt.colorbar(boop)
+	##########################################
+	fig2 = plt.figure()
+	ax2 = fig2.add_subplot(111, projection='3d', axisbg = background)
+	ax2.set_title("Percentage of articulating time steps")
+	articulate_values = np.loadtxt("features_property_values_articulate.v", delimiter=",")
+	colors = [element for element in articulate_values]
+	boop = ax2.scatter(graph_data[:,0], graph_data[:,1], graph_data[:,2], c=colors, cmap=point_color, marker='o', edgecolors='none')
+	ax2.set_xlabel('PCA[0] magnitude')
+	ax2.set_ylabel('PCA[1] magnitude')
+	ax2.set_zlabel('PCA[2] magnitude')
+	plt.colorbar(boop)
+	plt.show()
+	#################################
+	projectedQueueMatrix = results.project(referenceQueueMatrix)
+	projectedQueueMatrix = sp.special.expit(projectedQueueMatrix)
+	sigmoidedResults = sp.special.expit(final_components)
+	np.savetxt("sValues.m", results.s, delimiter=",")
+	np.savetxt("projectedQueueMatrix.m", projectedQueueMatrix, delimiter=",")
+	np.savetxt("pcaResults.m", sigmoidedResults, delimiter=",")
+
 #######################################
+
+'''
+General Graph Functions
+'''
 def plot_figure_colors(title, data_list, input_file, xlabel = 'PCA[0] magnitude', ylabel = 'PCA[1] magnitude', zlabel = 'PCA[2] magnitude'):
+	'''plots a color-coded, three-dimensional numpy graph'''
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d', axisbg = background)
 	ax.set_title(title)
@@ -115,6 +205,7 @@ def plot_figure_colors(title, data_list, input_file, xlabel = 'PCA[0] magnitude'
 	plt.show()
 
 def plot_figure(title, data_list, input_file, xlabel = 'PCA[0] magnitude', ylabel = 'PCA[1] magnitude', zlabel = 'PCA[2] magnitude'):
+	'''plots a  three-dimensional numpy graph with no particular color-coding scheme'''
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d', axisbg = background)
 	ax.set_title(title)
@@ -124,12 +215,3 @@ def plot_figure(title, data_list, input_file, xlabel = 'PCA[0] magnitude', ylabe
 	ax.set_zlabel(zlabel)
 	plt.colorbar(boop)
 	plt.show()
-
-#################################
-
-projectedQueueMatrix = results.project(referenceQueueMatrix)
-projectedQueueMatrix = sp.special.expit(projectedQueueMatrix)
-sigmoidedResults = sp.special.expit(final_components)
-np.savetxt("sValues.m", results.s, delimiter=",")
-np.savetxt("projectedQueueMatrix.m", projectedQueueMatrix, delimiter=",")
-np.savetxt("pcaResults.m", sigmoidedResults, delimiter=",")
